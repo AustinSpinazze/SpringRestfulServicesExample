4a35adb5c6982671307fe2aa6a99bc315b676c20
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-04-07 08:35:06 -0500

    Initial commit.

---
+++ src/main/java/AustinSpinazze/SpringRestfulServices/restfulwebservicesexample/RestfulWebServicesExampleApplication.java
@@ -0,0 +1,13 @@
+package AustinSpinazze.SpringRestfulServices.restfulwebservicesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AustinSpinazze/SpringRestfulServices/restfulwebservicesexample/RestfulWebServicesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package AustinSpinazze.SpringRestfulServices.restfulwebservicesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
